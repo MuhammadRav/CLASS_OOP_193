4590b161a926701b384eccd3fbd7536b0a03724c
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-04-10 14:42:04 +0700

    p

--- OOP_1/OOP_1.cpp
+++ OOP_1/OOP_1.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
-
+class Mahasiswa 
+{
+public:
+	int NIM;
+	string nama;
+	void tampi;()
+	{
+		cout << "NIM = " << nim;
+		cout << "\nNama = " << nama;
+	}
+};
 
 int main()
 {
