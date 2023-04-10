796fa133296ab717a060026dc7657df23ef2b140
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-04-10 15:32:07 +0700

    menambah comment

--- OOP_1/OOP_1.cpp
+++ OOP_1/OOP_1.cpp
@@ -21,7 +21,7 @@ private:			// Akses Modifier
 	string kode;
 	string namaMk;
 public:				// Akses modifier
-	void inputMk()			//Prosedur / Metode / Fungsi
+	void inputMk()			//Prosedur / Metode / Fungsi => bagian dari Member
 	{
 		cout << "========================\n";
 		cout << "\nMasukkan jumlah SKS = ";
@@ -32,7 +32,7 @@ public:				// Akses modifier
 		cin >> namaMk;
 	}
 
-	void tampilMk()			//Prosedur / Metode / Fungsi
+	void tampilMk()			//Prosedur / Metode / Fungsi => bagian dari Member
 	{
 		cout << "=====================\n";
 		cout << "\nJumlah SKS = " << sks;
@@ -57,3 +57,8 @@ int main()
 	Mk.tampilMk();
 	
 }
+
+// Bagian dari Member:
+// 1. Konstanta
+// 2. Variable
+// 3. Fungsi/Procedur (Metode)
