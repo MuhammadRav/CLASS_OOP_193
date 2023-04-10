1a8652708c8abe7fb09ed7aec2a77a4093484a99
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2023-04-10 15:00:49 +0700

    aq

--- OOP_1/OOP_1.cpp
+++ OOP_1/OOP_1.cpp
@@ -21,14 +21,22 @@ private:
 	string namaMk;
 public:
 	void inputMk()
-		{
-			cout << "\nMasukkan jumlah SKS = ";
-			cin >> sks;
-			cout << "\nMasukkan Kode MK = ";
-			cin >> kode;
-			cout << "\nMasukkan Nama MK = ";
-			cin >> namaMk;
-		}
+	{
+		cout << "\nMasukkan jumlah SKS = ";
+		cin >> sks;
+		cout << "\nMasukkan Kode MK = ";
+		cin >> kode;
+		cout << "\nMasukkan Nama MK = ";
+		cin >> namaMk;
+	}
+
+	void tampilMk()
+	{
+		cout << "\nJumlah SKS = " << sks;
+		cout << "\nKode MK = " << kode;
+		cout << "\nNama MK = " << namaMk << endl;
+
+	}
 };
 
 int main()
